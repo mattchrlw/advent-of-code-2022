b3008dd6ed0f4354d7db450b7fa24428d13be16b
Matthew Low <mlow@atlassian.com>
2022-12-02 08:55:48 +1100

    Formatted

--- day/01.py
+++ day/01.py
@@ -1,19 +1,22 @@
-f = open('test/01.txt', 'r')
+f = open("test/01.txt", "r")
 text = f.read()
 
+
 def part1(text):
-	cals = text.split("\n\n")
-	new_cals = []
-	for cal in cals:
-		new_cals.append(sum([int(i) for i in cal.split("\n")]))
-	return max(new_cals)
+    cals = text.split("\n\n")
+    new_cals = []
+    for cal in cals:
+        new_cals.append(sum([int(i) for i in cal.split("\n")]))
+    return max(new_cals)
+
 
 def part2(text):
-	cals = text.split("\n\n")
-	new_cals = []
-	for cal in cals:
-		new_cals.append(sum([int(i) for i in cal.split("\n")]))
-	return sum(sorted(new_cals, reverse=True)[0:3])
+    cals = text.split("\n\n")
+    new_cals = []
+    for cal in cals:
+        new_cals.append(sum([int(i) for i in cal.split("\n")]))
+    return sum(sorted(new_cals, reverse=True)[0:3])
+
 
 print(part1(text))
 print(part2(text))
