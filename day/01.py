43f5cf4d96daddea0b32ab3a81e684d1cc3f4b7d
Matthew Low <mlow@atlassian.com>
2022-12-02 08:53:52 +1100

    Day 1

---
+++ day/01.py
@@ -0,0 +1,19 @@
+f = open('test/01.txt', 'r')
+text = f.read()
+
+def part1(text):
+	cals = text.split("\n\n")
+	new_cals = []
+	for cal in cals:
+		new_cals.append(sum([int(i) for i in cal.split("\n")]))
+	return max(new_cals)
+
+def part2(text):
+	cals = text.split("\n\n")
+	new_cals = []
+	for cal in cals:
+		new_cals.append(sum([int(i) for i in cal.split("\n")]))
+	return sum(sorted(new_cals, reverse=True)[0:3])
+
+print(part1(text))
+print(part2(text))
